b304bccddd90b8431a7c11dbfe295a737083f3b0
Bernhard Frauendienst <git@nospam.obeliks.de>
2020-05-04 01:34:22 +0200

    wip weight min/max

--- phoehnlix-frontend/webpack.config.d/01-import-webpack.js
+++ phoehnlix-frontend/webpack.config.d/01-import-webpack.js
@@ -1,3 +1,5 @@
 //import webpack from 'webpack'
-const webpack = require('webpack'); 
+
+// TODO: webpack.config.d are not sorted until kotlin 1.4 https://youtrack.jetbrains.com/issue/KT-37240
+//const webpack = require('webpack');
 
